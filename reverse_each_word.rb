b04023377573c702ee5c32886e24039b77e10da8
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-07 23:34:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   array = sentence.split
   reverse_array = []
-  reverse_array = array.collect |word| do
+  reverse_array = array.collect do |word| 
     word.reverse
   end 
   reverse_array.join(" ")
